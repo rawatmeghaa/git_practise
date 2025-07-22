17c6a6c7210791dd5b8721c3e8d3f189e5cc9e9e
Megha Rawat <megha.rawat@Centilytics.com>
2025-07-22 10:55:09 +0530

    resolving issue

--- pre_check.py
+++ pre_check.py
@@ -1,4 +1,7 @@
-def my_function():
-    a=2
-    b=5
+"""This module contains a sample function for demonstration."""
 
+def my_function():
+    """Demonstrates a simple operation using variables."""
+    a = 2
+    b = 5
+    return a + b
